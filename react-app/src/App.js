f09b147dcf71baa206e2bacda4f1d479cdebb84c
Katie <katie.snell@student.manchester.ac.uk>
2019-11-12 10:31:37 +0000

    Added reusable Widget and Button components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
+import PropTypes from 'prop-types'
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'This is the state txt'
+    }
+  }
+
+  update( e ) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
 
   render() {
     let txt = this.props.txt
-    return <h1>{txt}</h1>
+    return (
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+        <Button>I <Heart/> React</Button>
+      </div>
+    )
   }
 }
+
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
+}
+
+const Widget = (props) => {
+  return <input type="text" onChange={props.update} />
+}
+
+const Button = (props) => {
+  return <button>{props.children}</button>
+}
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "This is the default txt"
+}
+
 export default App
